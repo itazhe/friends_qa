b6ea367c03d390b71b3de1bdd3b43a348c0e5f5c
azhe <602661651@qq.com>
2020-07-01 16:53:28 +0800

    第一次提交，完成了小程序发部分页面

---
+++ 1/pages/index2/index2.js
@@ -0,0 +1,53 @@
+// pages/index2/index2.js
+const app = getApp()
+
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    array: ['请选择','10道题', '8道题', '5道题'],
+    index: 0
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  bindpickerchange(e) {
+    this.setData({
+      index: e.detail.value
+    })
+  }
+})
---
+++ 1/pages/index4/index4.js
@@ -0,0 +1,7 @@
+// pages/index4/index4.js
+Page({
+  data: {
+    data1: 2,
+    data2: 10,
+  }
+})
